8cbd9088a89ca9adaf28338226248b0bbb3ca51d
Alesso Morris <145225716+alesso-m@users.noreply.github.com>
2024-01-04 15:53:16 -0800

    Added JavaScript functions for adding ,removing, and saving tasks
    Stores updated task list data in local storage.

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write a nonempty task!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox.value = '';
+
+}
+
+listContainer.addEventListener("click", function(e){
+        if (e.target.tagName === "LI") { //if list item clicked
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName === "SPAN") { //if x icon clicked
+            e.target.parentElement.remove();
+            saveData();
+        }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
